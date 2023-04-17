9de0125e9679c7b7a77ab0a91d6544674cc3fae1
Ivan Meirelles <hackermeirelles+github@gmail.com>
2023-04-17 18:50:05 -0300

    while

--- JavaScript_Youtube/curso_em_video/Aula013/ambiente.js
+++ JavaScript_Youtube/curso_em_video/Aula013/ambiente.js
@@ -1,9 +1,20 @@
+/*Estrutura de repetição com teste no final*/
 var i = 1
+do {
+    console.log(`Passo ${i}`)
+    i++
+} while (i <= 10)
+
+
+
+/*Estrutura de repetição com teste no inicio*/
+/*var i = 1
 
 while (i <= 10) {
-    console.log(`Tudo bem!`)
+    console.log(`Passo ${i}`)
     i++
-}
+}*/
+
 
 /*console.log(`Tudo bem!`)
 console.log(`Tudo bem!`)
