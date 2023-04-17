af5c2711ab0d2455ee134e8e9eef2b0e6f104e1b
Ivan Meirelles <hackermeirelles+github@gmail.com>
2023-04-17 17:36:27 -0300

    While

---
+++ JavaScript_Youtube/curso_em_video/Aula013/ambiente.js
@@ -0,0 +1,14 @@
+var i = 1
+
+while (i <= 10) {
+    console.log(`Tudo bem!`)
+    i++
+}
+
+/*console.log(`Tudo bem!`)
+console.log(`Tudo bem!`)
+console.log(`Tudo bem!`)
+console.log(`Tudo bem!`)
+console.log(`Tudo bem!`)
+console.log(`Tudo bem!`)
+*/
