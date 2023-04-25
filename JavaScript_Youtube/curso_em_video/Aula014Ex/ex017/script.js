9c9be87b9186e9223122a56b9b5d734e1794df4b
Ivan Meirelles <hackermeirelles+github@gmail.com>
2023-04-24 22:26:13 -0300

    Update script.js

--- JavaScript_Youtube/curso_em_video/Aula014Ex/ex017/script.js
+++ JavaScript_Youtube/curso_em_video/Aula014Ex/ex017/script.js
@@ -1,14 +1,23 @@
 function gerarTabuada() {
     let num = document.querySelector(`input#txtnum`)
     let tab = document.querySelector(`select#seltab`)
-
     let res = document.querySelector(`div#res`)
 
     if (num.value.length == 0) {
         alert(`Por favor, digite um número!`)
     } else {
         let n = Number(num.value)
+        let c = 1
 
+        tab.innerHTML = ``
+        for (c = 1; c <= 10; c++) {
+            let item = document.createElement(`option`)
+            item.text = `${n} x ${c} = ${n * c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+
+        /*
         let c = 1
         tab.innerHTML = ``
         while (c <= 10) {
@@ -17,7 +26,7 @@ function gerarTabuada() {
             item.value = `tab${c}`
             tab.appendChild(item)
             c++
-        }
+        }*/
 
     }
 }
